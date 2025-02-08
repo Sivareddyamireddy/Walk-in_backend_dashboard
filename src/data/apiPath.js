d73819ff378b13e0d3e9f083df3b74b581653c9c
SIVAREDDY AMIREDDY <109684811+Sivareddyamireddy@users.noreply.github.com>
2025-02-08 20:07:38 +0530

    api path changed

--- src/data/apiPath.js
+++ src/data/apiPath.js
@@ -1,5 +1,5 @@
 
-export const API_URL = "http://localhost:4000"
+//export const API_URL = "http://localhost:4000"
 
-//const API_URL = "https://walk-in-backend.onrender.com"
+export const API_URL = "https://walk-in-backend.onrender.com"
 
